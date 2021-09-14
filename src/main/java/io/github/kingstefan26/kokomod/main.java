2d8f08fc3aedda64167e79940af1df0de5a7a568
kokoniara <eryk.ruta1@gmail.com>
2021-09-14 23:31:47 +0200

    gradle! :)

--- src/main/java/io/github/kingstefan26/kokomod/main.java
+++ src/main/java/io/github/kingstefan26/kokomod/main.java
@@ -9,7 +9,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 @Mod(modid = main.MODID, version = main.VERSION)
 public class main {
     public static final String MODID = "kokoMod";
-    public static final String VERSION = "0.2.8-APLHA";
+    public static final String VERSION = "0.2.8-ALPHA";
     public static final boolean debug = true;
 
     @EventHandler
