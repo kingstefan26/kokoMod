1ca7715c9a59bc7d4942a3401ef270aa433ece4f
kokoniara <eryk.ruta1@gmail.com>
2021-09-16 14:17:52 +0200

    new draw line (from dankers mod)

--- src/main/java/io/github/kingstefan26/kokomod/module/misc/test/test.java
+++ src/main/java/io/github/kingstefan26/kokomod/module/misc/test/test.java
@@ -11,30 +11,55 @@ import io.github.kingstefan26.kokomod.module.macro.macroUtil.macroStages;
 import io.github.kingstefan26.kokomod.util.renderUtil.hehe;
 import io.github.kingstefan26.kokomod.util.sendChatMessage;
 import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.Vec3;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
+
+import static io.github.kingstefan26.kokomod.util.renderUtil.draw3Dline.draw3DLine;
 
 
 public class test extends Module {
     public test() {
         super("test", "test", Category.MISC, true, " hello test", " rip test");
-        SettingsManager.getSettingsManager().rSetting(new Setting("interpolate steps", this, 10, 1, 60, true));
-        SettingsManager.getSettingsManager().rSetting(new Setting("wanted Yaw", this, 90, 1, 90, false));
-        SettingsManager.getSettingsManager().rSetting(new Setting("wanted Pitch", this, 1, 1, 90, false));
+//        SettingsManager.getSettingsManager().rSetting(new Setting("interpolate steps", this, 10, 1, 60, true));
+//        SettingsManager.getSettingsManager().rSetting(new Setting("wanted Yaw", this, 90, 1, 90, false));
+//        SettingsManager.getSettingsManager().rSetting(new Setting("wanted Pitch", this, 1, 1, 90, false));
     }
-boolean firstTime = false;
-    long interpolateTimer;
-    int stepCount = 100;
-    int currentStep = 1;
-    float wantedPitch, wantedYaw;
-    float enableYaw, enablePitch;
-    float diffranceYaw, diffrancePitch;
+//    boolean firstTime = false;
+//    long interpolateTimer;
+//    int stepCount = 100;
+//    int currentStep = 1;
+//    float wantedPitch, wantedYaw;
+//    float enableYaw, enablePitch;
+//    float diffranceYaw, diffrancePitch;
+
+    float wantedX = 0, wantedY = 10 , wantedZ = 0;
 
 
     @SubscribeEvent
     public void onWorldRender(RenderWorldLastEvent e) {
         hehe.drawTextAtWorld("hehe", 0, 10, 0, Integer.parseInt("47a8ed", 16), 0.10F, false, true, e.partialTicks);
+        BlockPos desiredlocation = new BlockPos(wantedX,wantedY,wantedZ);
+        BlockPos playerlocation = mc.thePlayer.getPosition();
+        //line one
+        Vec3 pos1;
+        Vec3 pos2;
+        //line two
+        Vec3 pos3;
+        Vec3 pos4;
+
+        pos1 = new Vec3(playerlocation.getX(),playerlocation.getY(),playerlocation.getZ());
+        pos2 = new Vec3(desiredlocation.getX(),playerlocation.getY(),desiredlocation.getZ());
+
+        pos3 = new Vec3(desiredlocation.getX(),playerlocation.getY(),desiredlocation.getZ());
+        pos4 = new Vec3(desiredlocation.getX(),desiredlocation.getY(),desiredlocation.getZ());
+
+        draw3DLine(pos1,pos2, 0x47a8ed, 5, false, e.partialTicks);
+
+        draw3DLine(pos3,pos4, 0x47a8ed, 5, false, e.partialTicks);
+
 
     }
 
@@ -70,10 +95,10 @@ boolean firstTime = false;
     @Override
     public void onEnable() {
         super.onEnable();
-        sendChatMessage.sendClientMessage("bye reaload!!!", true);
-        lastLeftOff.getLastLeftOff().registerLastLeftOff(new lastleftoffObject(10, 10, 10, cropType.WART, macroStages.DEFAULT));
-        mc.displayGuiScreen(new newergui());
-        KeyBinding.setKeyBindState(mc.gameSettings.keyBindAttack.getKeyCode(), true);
+//        sendChatMessage.sendClientMessage("bye reaload!!!", true);
+//        lastLeftOff.getLastLeftOff().registerLastLeftOff(new lastleftoffObject(10, 10, 10, cropType.WART, macroStages.DEFAULT));
+//        mc.displayGuiScreen(new newergui());
+//        KeyBinding.setKeyBindState(mc.gameSettings.keyBindAttack.getKeyCode(), true);
 
 //        firstTime = true;
 //        interpolateTimer = System.currentTimeMillis();
