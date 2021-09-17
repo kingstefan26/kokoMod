078aa22182554ca11fc26afc2246c97206b05a05
kokoniara <eryk.ruta1@gmail.com>
2021-09-17 17:22:28 +0200

    debug when -Dkokomod.debug=true on VM arguments

--- src/main/java/io/github/kingstefan26/kokomod/main.java
+++ src/main/java/io/github/kingstefan26/kokomod/main.java
@@ -8,18 +8,25 @@ import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.lwjgl.Sys;
+
+import java.util.Objects;
 
 @Mod(modid = main.MODID, version = main.VERSION)
 public class main {
     public static final String MODID = "kokoMod";
     public static final String VERSION = "0.2.8-ALPHA";
-    public static final boolean debug = true;
+    public static boolean debug = false;
+
+    public main(){
+        String a = System.getProperty("kokomod.debug","false");
+        if(Objects.equals(a, "true")) {
+            debug = true;
+        }
+    }
 
     @EventHandler
     public void preInit(final FMLPreInitializationEvent event) {
-        if(debug){
-            System.getProperty("devauth.enabled","true");
-        }
         for(CommandBase c : commandIndex.getCommandIndex().getCommands()){
             ClientCommandHandler.instance.registerCommand(c);
         }
