6a848e37856ecfecdf5e83990a44652dacd81a80
kokoniara <eryk.ruta1@gmail.com>
2021-09-22 17:40:52 +0200

    added a logger + new test achivments

--- src/main/java/io/github/kingstefan26/kokomod/main.java
+++ src/main/java/io/github/kingstefan26/kokomod/main.java
@@ -3,11 +3,18 @@ package io.github.kingstefan26.kokomod;
 import io.github.kingstefan26.kokomod.core.commands.commandIndex;
 import io.github.kingstefan26.kokomod.core.kokoMod;
 import io.github.kingstefan26.kokomod.util.renderUtil.updateWidowTitle;
+
 import net.minecraft.command.CommandBase;
+
+import net.minecraft.init.Items;
+import net.minecraft.stats.Achievement;
 import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.Objects;
 
@@ -16,6 +23,9 @@ public class main {
     public static final String MODID = "kokoMod";
     public static final String VERSION = "0.2.8-ALPHA";
     public static boolean debug = false;
+
+    public static final Logger logger = LogManager.getLogger();
+    public static Achievement cockbone;
 
     public main(){
         String a = System.getProperty("kokomod.debug","false");
@@ -29,6 +39,7 @@ public class main {
         for(CommandBase c : commandIndex.getCommandIndex().getCommands()){
             ClientCommandHandler.instance.registerCommand(c);
         }
+        cockbone = (new Achievement("a", "cock with no cock bone", 0, 0, Items.bone, (Achievement)null)).registerStat();
     }
 
     @EventHandler
