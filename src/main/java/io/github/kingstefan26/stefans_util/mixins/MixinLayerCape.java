bacdfeb6135f1c149c55219dd3fae9009b4b6980
kingstefan26 <eryk.ruta1@gmail.com>
2022-09-25 01:16:45 +0200

    some changes to cape fix + added TestAValue to test unravel the mysteries of the obfuscated code

--- src/main/java/io/github/kingstefan26/stefans_util/mixins/MixinLayerCape.java
+++ src/main/java/io/github/kingstefan26/stefans_util/mixins/MixinLayerCape.java
@@ -4,6 +4,7 @@
 
 package io.github.kingstefan26.stefans_util.mixins;
 
+import io.github.kingstefan26.stefans_util.module.debug.TestAValue;
 import io.github.kingstefan26.stefans_util.module.wip.CapeFix;
 import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.renderer.GlStateManager;
@@ -23,57 +24,106 @@ public abstract class MixinLayerCape implements LayerRenderer<AbstractClientPlay
     private RenderPlayer playerRenderer;
 
 
+    /**
+     * Most of this function is copy-paste (mojang plz don't send cease and desist cuz copyright)
+     * cuz tbh I didn't feel like doing callback injectors so just override the method, right?
+     *
+     * @param playerIn     THE PLAYER WE ARE RENDERING
+     * @param partialTicks For tick interpolation, so we update rendering more than tick rate
+     * @param scale        IDK
+     */
     @Override
-    public void doRenderLayer(AbstractClientPlayer entitylivingbaseIn, float p_177141_2_, float p_177141_3_, float partialTicks, float p_177141_5_, float p_177141_6_, float p_177141_7_, float scale) {
-        if (entitylivingbaseIn.hasPlayerInfo() && !entitylivingbaseIn.isInvisible() && entitylivingbaseIn.isWearing(EnumPlayerModelParts.CAPE) && entitylivingbaseIn.getLocationCape() != null) {
-            GlStateManager.color(1F, 1.0F, 1.0F, 1.0F);
-            this.playerRenderer.bindTexture(entitylivingbaseIn.getLocationCape());
-            GlStateManager.pushMatrix();
-            GlStateManager.translate(0.0F, 0.0F, 0.125F);
-            double d0 = entitylivingbaseIn.prevChasingPosX + (entitylivingbaseIn.chasingPosX - entitylivingbaseIn.prevChasingPosX) * (double) partialTicks - (entitylivingbaseIn.prevPosX + (entitylivingbaseIn.posX - entitylivingbaseIn.prevPosX) * (double) partialTicks);
-            double d1 = entitylivingbaseIn.prevChasingPosY + (entitylivingbaseIn.chasingPosY - entitylivingbaseIn.prevChasingPosY) * (double) partialTicks - (entitylivingbaseIn.prevPosY + (entitylivingbaseIn.posY - entitylivingbaseIn.prevPosY) * (double) partialTicks);
-            double d2 = entitylivingbaseIn.prevChasingPosZ + (entitylivingbaseIn.chasingPosZ - entitylivingbaseIn.prevChasingPosZ) * (double) partialTicks - (entitylivingbaseIn.prevPosZ + (entitylivingbaseIn.posZ - entitylivingbaseIn.prevPosZ) * (double) partialTicks);
-            float f = entitylivingbaseIn.prevRenderYawOffset + (entitylivingbaseIn.renderYawOffset - entitylivingbaseIn.prevRenderYawOffset) * partialTicks;
-            double d3 = (double) MathHelper.sin(f * (float) Math.PI / 180.0F);
-            double d4 = (double) (-MathHelper.cos(f * (float) Math.PI / 180.0F));
-            float f1 = (float) d1 * 10.0F;
-            f1 = MathHelper.clamp_float(f1, -6.0F, 32.0F);
-            float f2 = (float) (d0 * d3 + d2 * d4) * 100.0F;
-            float f3 = (float) (d0 * d4 - d2 * d3) * 100.0F;
-
-            if (f2 < 0.0F) {
-                f2 = 0.0F;
-            }
-
-            float f4 = entitylivingbaseIn.prevCameraYaw + (entitylivingbaseIn.cameraYaw - entitylivingbaseIn.prevCameraYaw) * partialTicks;
-
-            // sin( The distance walked a tick ago + the diffrence bettwen distance walked now and then interpolated for ticks * 6 ) * 32 * change that when player moves around
-            // this is the rate of "change" in perspective to walking its -6 flying up 32 max
-            // negative values = go up or go down idk
-            f1 = f1 + MathHelper.sin((entitylivingbaseIn.prevDistanceWalkedModified + (entitylivingbaseIn.distanceWalkedModified - entitylivingbaseIn.prevDistanceWalkedModified) * partialTicks) * 6.0F) * 32.0F * f4;
-
-            if (entitylivingbaseIn.isSneaking()) {
-                f1 += 25.0F;
-            }
+    public void doRenderLayer(AbstractClientPlayer playerIn, float idk, float idk1, float partialTicks, float idk2, float idk3, float idk4, float scale) {
+        if (!playerIn.hasPlayerInfo()) return;
+        if (playerIn.isInvisible()) return;
+        if (!playerIn.isWearing(EnumPlayerModelParts.CAPE)) return;
+        if (playerIn.getLocationCape() == null) return;
 
 
-            Float tempprary = 6.0F + f2 / 2.0F + f1;
-
-            if (tempprary >= 90f && CapeFix.enabled)
-                tempprary = 90F;
-
-
-            // temprotaty is the accual rotate value, 6 at rest 0 at flacid ~165 at erect
-//            newFrameWorkTest.TEXT = "CURUSTY DOG SAID: " + tempprary;
+        // CAPE BEHAVIOR
+        // it bobs up and down when walking in all directions
+        // if we walk sideways it goes the opposite way
+        // moves with shift
+        // the faster we walk forward more straight it gets
+        //
 
 
-            GlStateManager.rotate(tempprary, 1.0F, 0.0F, 0.0F);
-//            GlStateManager.rotate(6.0F + f2 / 2.0F + f1, 1.0F, 0.0F, 0.0F);
-            GlStateManager.rotate(f3 / 2.0F, 0.0F, 0.0F, 1.0F);
-            GlStateManager.rotate(-f3 / 2.0F, 0.0F, 1.0F, 0.0F);
-            GlStateManager.rotate(180.0F, 0.0F, 1.0F, 0.0F);
-            this.playerRenderer.getMainModel().renderCape(0.0625F);
-            GlStateManager.popMatrix();
+        GlStateManager.color(1F, 1.0F, 1.0F, 1.0F);
+        this.playerRenderer.bindTexture(playerIn.getLocationCape());
+        GlStateManager.pushMatrix();
+        GlStateManager.translate(0.0F, 0.0F, 0.125F);
+
+        double realX = playerIn.prevChasingPosX + (playerIn.chasingPosX - playerIn.prevChasingPosX) * partialTicks - (playerIn.prevPosX + (playerIn.posX - playerIn.prevPosX) * partialTicks);
+        double realY = playerIn.prevChasingPosY + (playerIn.chasingPosY - playerIn.prevChasingPosY) * partialTicks - (playerIn.prevPosY + (playerIn.posY - playerIn.prevPosY) * partialTicks);
+        double realZ = playerIn.prevChasingPosZ + (playerIn.chasingPosZ - playerIn.prevChasingPosZ) * partialTicks - (playerIn.prevPosZ + (playerIn.posZ - playerIn.prevPosZ) * partialTicks);
+        float realYawOffSet = playerIn.prevRenderYawOffset + (playerIn.renderYawOffset - playerIn.prevRenderYawOffset) * partialTicks;
+
+        double d3 = MathHelper.sin(realYawOffSet * (float) Math.PI / 180.0F);
+        double d4 = (-MathHelper.cos(realYawOffSet * (float) Math.PI / 180.0F));
+
+
+        // d3 d4 realYawOffSet dont seem to go out of norm when insane speed
+
+
+        // this appers to control modyfiing cape when flying and the bobbing behavior
+        float f1 = (float) realY * 10.0F;
+        f1 = MathHelper.clamp_float(f1, -6.0F, 32.0F);
+
+        // and this one the one corralled with speed
+        float f2 = (float) (realX * d3 + realZ * d4) * 100.0F;
+
+        // this contros the side to side movment
+        float f3 = (float) (realX * d4 - realZ * d3) * 100.0F;
+
+
+        // this makes so the cape doesn't fly into you when walking back
+        if (f2 < 0.0F) {
+            f2 = 0.0F;
         }
+
+        if (CapeFix.enabled) {
+            f2 = MathHelper.clamp_float(f2, -150F, 150F);
+            f3 = MathHelper.clamp_float(f3, -150F, 150F);
+        }
+
+
+        float realYaw = playerIn.prevCameraYaw + (playerIn.cameraYaw - playerIn.prevCameraYaw) * partialTicks;
+
+        // sin( The distance walked a tick ago + the difference between distance walked now and then interpolated for ticks * 6 ) * 32 * change that when player moves around
+        // this is the rate of "change" in perspective to walking its -6 flying up 32 max
+        // -6 = sit flush with player,,,, 32 stay away from player
+        // WHEN PLAYER IS FLYING UP AND WALKING FORWARD f1 IS -6  CAPE IS FLYTING UP INTO THE HEAD WE DONT WANT THAT
+        // IT SHOULD BE POINTING BACKWARDS NOT INTO THE HEAD
+        f1 = f1 + MathHelper.sin((playerIn.prevDistanceWalkedModified + (playerIn.distanceWalkedModified - playerIn.prevDistanceWalkedModified) * partialTicks) * 6.0F) * 32.0F * realYaw;
+
+        if (playerIn.isSneaking()) {
+            f1 += 25.0F;
+        }
+
+        TestAValue.ToDraw[0] = "f1: " + f1;
+        TestAValue.ToDraw[1] = "f2: " + f2;
+        TestAValue.ToDraw[2] = "f3: " + f3;
+
+
+        // THIS PART IS MINE
+        // temporality is the actual rotate value, 6 at rest 0 at flaccid, ~165 at erect
+        float temporary = 6.0F + f2 / 2.0F + f1;
+
+        if (temporary >= 90f && CapeFix.enabled) {
+//            temporary = 90F;
+
+            temporary = (float) (temporary + (Math.log(temporary) * 5));
+        }
+
+        // END OF MINE
+
+
+        GlStateManager.rotate(temporary, 1.0F, 0.0F, 0.0F);
+        GlStateManager.rotate(f3 / 2.0F, 0.0F, 0.0F, 1.0F);
+        GlStateManager.rotate(-f3 / 2.0F, 0.0F, 1.0F, 0.0F);
+        GlStateManager.rotate(180.0F, 0.0F, 1.0F, 0.0F);
+        this.playerRenderer.getMainModel().renderCape(0.0625F);
+        GlStateManager.popMatrix();
+
     }
 }
---
+++ src/main/java/io/github/kingstefan26/stefans_util/module/debug/TestAValue.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2022. All copyright reserved
+ */
+
+package io.github.kingstefan26.stefans_util.module.debug;
+
+import io.github.kingstefan26.stefans_util.core.module.moduleframes.prototypeModule;
+import io.github.kingstefan26.stefans_util.util.CustomFont;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+
+import java.awt.*;
+
+public class TestAValue extends prototypeModule {
+    public static String[] ToDraw = new String[]{"", "", ""};
+    CustomFont c = new CustomFont(new Font("JetBrains Mono", Font.BOLD, 20), 20);
+
+
+    public TestAValue() {
+        super("TestAValue");
+    }
+
+    @Override
+    public void onGuiRender(RenderGameOverlayEvent e) {
+        if (e.type != RenderGameOverlayEvent.ElementType.TEXT) return;
+
+        int a = 10;
+        for (String s : ToDraw) {
+            a += c.getStringHeight(s) + 2;
+            c.drawString(s, 100, a, 0xFFFFFFFF, 1);
+        }
+    }
+
+    @Override
+    protected void PROTOTYPETEST() {
+
+    }
+}
