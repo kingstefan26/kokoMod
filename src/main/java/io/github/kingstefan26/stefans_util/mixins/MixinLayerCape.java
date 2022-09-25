48084498b4a75c53d828860298780208d6f163ae
kingstefan26 <eryk.ruta1@gmail.com>
2022-09-25 17:40:21 +0200

    made the mixin redirect instead of overriding the method to make it safer and more conflict free

--- src/main/java/io/github/kingstefan26/stefans_util/mixins/MixinLayerCape.java
+++ src/main/java/io/github/kingstefan26/stefans_util/mixins/MixinLayerCape.java
@@ -8,28 +8,23 @@ import io.github.kingstefan26.stefans_util.module.wip.CapeFix;
 import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.renderer.entity.RenderPlayer;
 import net.minecraft.client.renderer.entity.layers.LayerCape;
-import net.minecraft.client.renderer.entity.layers.LayerRenderer;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = LayerCape.class)
-public abstract class MixinLayerCape implements LayerRenderer<AbstractClientPlayer> {
+public class MixinLayerCape {
     @Final
     @Shadow
     private RenderPlayer playerRenderer;
 
 
-    /**
-     * Most of this function is copy-paste (mojang plz don't send cease and desist cuz copyright)
-     * cuz tbh I didn't feel like doing callback injectors so just override the method, right?
-     *
-     * @param playerIn     THE PLAYER WE ARE RENDERING
-     * @param partialTicks For tick interpolation, so we update rendering more than tick rate
-     * @param scale        IDK
-     */
-    @Override
-    public void doRenderLayer(AbstractClientPlayer playerIn, float idk, float idk1, float partialTicks, float idk2, float idk3, float idk4, float scale) {
-        CapeFix.rendercapeRedirect(playerIn, partialTicks, playerRenderer);
+    @Inject(method = "doRenderLayer(Lnet/minecraft/client/entity/AbstractClientPlayer;FFFFFFF)V", at = @At("HEAD"), cancellable = true)
+    public void redirectRendering(AbstractClientPlayer entitylivingbaseIn, float p_177141_2_, float p_177141_3_, float partialTicks, float p_177141_5_, float p_177141_6_, float p_177141_7_, float scale, CallbackInfo ci) {
+        CapeFix.rendercapeRedirect(entitylivingbaseIn, partialTicks, playerRenderer);
+        ci.cancel();
     }
 }
